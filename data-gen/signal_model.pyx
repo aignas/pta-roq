bac8f9bf98abbe21c5ce088c17a38e4a607b25b6
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-06-17 10:39:36 +0100

    Finish writing data generation functions
    The Python code is written, but there are many basic Cython
    optimizations left to be done. Also I need to write some Python "glue"
    code, which we would use to manipulate the data.
    
    TODO: write unit tests to check the functions I write, as otherwise
    there can be lots of bugs.
    
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- data-gen/signal_model.pyx
+++ data-gen/signal_model.pyx
@@ -17,7 +17,7 @@ class UnitVectors:
     def Omega (self):
         # define a unit vector \hat{\Omega} as shown in the (4)
         return np.array([
-            -sin(self.theta)*cos(self.theta),
+        -sin(self.theta)*cos(self.theta),
             -sin(self.theta)*sin(self.phi),
             -cos(self.theta)])
 
@@ -38,41 +38,44 @@ class UnitVectors:
 # This will be a Pulsar Array data structure and we access the data in it the lazy
 # way.
 class PulsarGrid:
-    def __init__ (self, N):
+    def __init__ (self, N, ranges):
         # These wil be polar coordinates of the pulsars
-        self.__R = np.random.rand(N*3)
-        self.__R = self.__R.reshape(N,3)
+        self.__R = np.random.rand(N,3)
 
-        # FIXME the 10 here needs to be a general length scale 5 as well
-        self.__R[0,:] = self.__R[0,:] * 10 + 5
-        # Angular Parts range from -pi,pi or 0,pi
-        self.__R[1,:] = (self.__R[1,:] - 0.5) * 2*np.pi
-        self.__R[2,:] = self.__R[2,:] * np.pi
+        # Transform the random number distributions to span the ranges defined by the
+        # user
+        # FIXME Is there a nicer way to write it?
+        for i in range(3):
+            self.__R[:,i] = self.__R[:,i] * abs(ranges[i,0] - ranges[i,1]) + ranges[i].min()
 
-    def getAngles (self, idx):
-        return self.__angles[:,idx]
+    def getAngles (self):
+        return self.__R[:,1:3]
 
     def getUnitVector (self, idx):
-        p = np.array([sin (self.__angles[0,idx]), 0, 0 ])
-        p[1] = p[0]
-        p[2] = np.cos (self.__angles[0,idx])
-        p[0] *= np.cos (self.__angles[1,idx])
-        p[1] *= np.sin (self.__angles[1,idx])
+        p = np.zeros(3)
+        p[0] =  sin (self.__R[idx,1])
+        p[1] =  p[0]
+        p[2] =  cos (self.__R[idx,1])
+        p[0] *= cos (self.__R[idx,2])
+        p[1] *= sin (self.__R[idx,2])
         return p
 
     def getLength (self, idx):
-        return self.__Length[idx]
+        return self.__R[idx,0]
 
 # define antenna pattern functions as in (9) and (10)
 # The F[0] term is Fplus and F[1] is Fcross
-def antennaPattern (theta, phi):
+def antennaPattern (theta, phi, u_p):
     # Initialise unit vector according to the given parameters
     v = UnitVectors(theta, phi)
 
-    F = np.array([0,0])
-    F[0] = 0.5 * ( np.dot(v.m,v.p)**2 - np.dot(v.n,v.p)**2 )/ \
-              ( 1 + np.dot(v.Omega,v.p))
-    F[1] = ( np.dot(v.m,v.p) * np.dot(v.n,v.p) )/( 1 + np.dot(v.Omega,v.p))
+    d1 = np.dot(v.m(),u_p)
+    d2 = np.dot(v.n(),u_p)
+    d3 = np.dot(v.Omega(),u_p)
+
+    F = np.zeros(2)
+    F[0] = 0.5 * ( d1 + d2 ) * ( d1 - d2 ) / ( 1 + d3 )
+    F[1] = ( d1 * d2 ) / ( 1 + d3 )
     
     return F
 
@@ -86,24 +89,24 @@ def Phi (t, omega0, M):
     # FIXME the Cornell et al. paper has a constant term here. I believe, that the
     #       constant term here should be as well, but I am wondering if the constant
     #       term is the same as \Phi_0
-    return 1./(32 * M**(5./3)) * ( omega0**(-5./3) - omega(t)**(-5./3))
+    return 1./(32 * M**(5./3)) * ( omega0**(-5./3) - omega(t, omega0, M)**(-5./3))
 
 # define the GW contributions to the timing residuals as in (12) and (13)
 # The first term is the plus, the second term is the cross as in the F function
-def gWContribution (t, iota, psi, zeta, Phi0):
-    a = sin( 2 * (Phi(t) - Phi0) ) * (1 + cos(iota)**2)
-    b = cos( 2 * (Phi(t) - Phi0) ) * cos(iota)
-    gw = np.array([0,0])
+def gWContribution (omega0, M, t, iota, psi, zeta, Phi0):
+    a = sin( 2 * (Phi(t, omega0, M) - Phi0) ) * (1 + cos(iota)**2)
+    b = cos( 2 * (Phi(t, omega0, M) - Phi0) ) * cos(iota)
+    gw = np.zeros(2)
 
-    gw[0] = zeta / omega(t)**(1./3) * ( - a * cos(2*psi) - 2 * sin(2*psi) )
-    gw[1] = zeta / omega(t)**(1./3) * ( - a * sin(2*psi) - 2 * cos(2*psi) )
+    gw[0] = zeta / omega(t, omega0, M)**(1./3) * ( - a * cos(2*psi) - 2 * sin(2*psi) )
+    gw[1] = zeta / omega(t, omega0, M)**(1./3) * ( - a * sin(2*psi) - 2 * cos(2*psi) )
 
     return gw
 
 
 # define the coefficients amplitudes as shown in the (18)
 def amplitude(zeta, iota, phi, psi):
-    a = np.array([ 0,0,0,0 ])
+    a = np.zeros(4)
     a[0] = zeta * ( (1 + cos(iota)**2) * cos (phi) * cos (2*psi) \
                 + 2 * cos(iota) * sin (phi) * sin (2*psi) )
 
@@ -120,23 +123,24 @@ def amplitude(zeta, iota, phi, psi):
 
 
 # Define the time dependent basis functions as shown in the equation (19)
-def basis (theta, phi, t):
-    A = np.arraf([0,0,0,0])
-    F = antennaPattern(theta, phi, t)
-    A[0] = F[0] * omega(t)**(-1./3) * sin (2 * Phi(t))
-    A[1] = F[0] * omega(t)**(-1./3) * cos (2 * Phi(t))
-    A[2] = F[1] * omega(t)**(-1./3) * sin (2 * Phi(t))
-    A[3] = F[1] * omega(t)**(-1./3) * cos (2 * Phi(t))
+def basis (omega0, M, theta, phi, t, u_p):
+    A = np.zeros(4)
+    F = antennaPattern(theta, phi, u_p)
+    A[0] = F[0] * omega(t, omega0, M)**(-1./3) * sin (2 * Phi(t, omega0, M))
+    A[1] = F[0] * omega(t, omega0, M)**(-1./3) * cos (2 * Phi(t, omega0, M))
+    A[2] = F[1] * omega(t, omega0, M)**(-1./3) * sin (2 * Phi(t, omega0, M))
+    A[3] = F[1] * omega(t, omega0, M)**(-1./3) * cos (2 * Phi(t, omega0, M))
 
     return A
 
 # Define the pulsar term as in the eq (17)
-def pulsar (t, zeta, iota, Phi0, psi, theta, phi, omega0, L):
+def pulsar (t, M, D, iota, Phi0, psi, theta, phi, omega0, L, u_p):
     v = UnitVectors(theta, phi)
-    tp = t - L * (1 + np.dot(v.Omega, v.p))
+    tp = t - L * (1 + np.dot(v.Omega(), u_p))
+    zeta = M**(5./3)/D
 
-    F = antennaPattern(theta, phi, t)
-    s = gWContribution (tp, iota, psi, zeta, Phi0)
+    F = antennaPattern(theta, phi, u_p)
+    s = gWContribution (omega0, M, tp, iota, psi, zeta, Phi0)
     return np.dot(F,s)
 
 # Define the noise term
@@ -146,9 +150,10 @@ def noise (t):
     return np.exp ( - np.random.rand()**2 / (2*var))
 
 # Define the residual as a function of parameters
-def residual (t, zeta, iota, Phi0, psi, theta, phi, omega0, L):
+def residual (t, M, D, iota, Phi0, psi, theta, phi, omega0, L, u_p):
+    zeta = M**(5./3)/D
     a = amplitude(zeta, iota, phi, psi)
-    A = basis (theta, phi, t)
-    p = pulsar (t, zeta, iota, Phi0, psi, theta, phi, omega0, L)
+    A = basis (omega0, M, theta, phi, t, u_p)
+    p = pulsar (t, M, D, iota, Phi0, psi, theta, phi, omega0, L, u_p)
     n = noise (t)
     return np.dot(a,A) + p + n
