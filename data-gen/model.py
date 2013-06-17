39c4c5f5cf70cd875fbb814752c41725bbf3c40e
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-06-17 13:00:42 +0100

    Add a simple data generator.
    This data generator minimally tests the code and can generate simple
    data (as for now). Things which should be done:
    
        - Add ability to define schedules for different pulsar, so that we
        have observations from different pulsars taken at different times.
    
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- data-gen/model.py
+++ data-gen/model.py
@@ -2,5 +2,33 @@
 
 import pyximport; pyximport.install()
 import signal_model as sm
+import numpy as np
 
+N = 8
+PulsarCoordinateRanges = np.array([ [10,40],[0.2,1.],[-0.5,0.5]])
+t_max = 10.
+t_N = 255
 
+# Define our pulsar grid
+pulsars = sm.PulsarGrid (N, PulsarCoordinateRanges)
+a = np.zeros((t_N,N))
+
+#def residual (t, zeta, iota, Phi0, psi, theta, phi, omega0, L, u_p):
+t = 5
+M = 1
+D = 20
+iota = 2
+Phi0 = 1
+psi = 2
+theta = 0.5
+phi = 0.2
+omega0 = 0.00004
+
+for i in range(t_N):
+  t = t_max/t_N * i
+  for j in range(N):
+    L = pulsars.getLength(j)
+    u_p = pulsars.getUnitVector(j)
+    a[i,j] = sm.residual (t, M, D, iota, Phi0, psi, theta, phi, omega0, L, u_p)
+
+print a
