e158a24c3cbea2c14c4d1d0f774ecd02328b8eb1
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-06-24 11:23:47 +0100

    Model similar data to the IPTA challenge.
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- data-gen/model.py
+++ data-gen/model.py
@@ -8,15 +8,19 @@ import numpy as np
 
 # I generate N pulsars in the solid angle defined by \theta and \phi randomly
 # scattered accross some length
-N = 8
+N = 36
 PulsarCoordinateRanges = np.array([ [10,40],[0.2,1.],[-0.5,0.5]])
 pulsarNoise = 0.003
 
+# Some time variables so that it is easier to read the code
+week = 7*24*3600
+yr = 52 * week
+
 # Stop the generation here
-t_final = 20*365.25*24*3600
+t_final = 5*yr
 # Interval when we do not take any measurements
-t_interval_min = 0.9*7*24*3600  # 0.9 week gap is minimum
-t_interval_max = 1.3*7*24*3600  # 1.3 week gap is maximum
+t_interval_min = 2 * week
+t_interval_max = 2 * week
 # Max number of timings
 N_t_max = 1
 N_t_min = 1
@@ -28,7 +32,7 @@ schedule = np.random.rand(N) * 0
 # Define our pulsar grid and the sources
 pulsars = sm.PulsarGrid (N, PulsarCoordinateRanges, pulsarNoise)
 sources = np.array([ sm.Source( M=5, D=10e9*5, iota=2, Phi0=1, psi=2,
-                                theta=0.5, phi=0.2, omega0=1e-8)
+                                theta=0.5, phi=0.2, omega0=2*np.pi*1e-8)
                     ])
 
 # Generate the actual data.
