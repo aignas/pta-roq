0e008a63eaaa09aeaa9acc6d1a43d19d3e167976
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-01 12:34:21 +0100

    Model: Adjust the pulsar positions on the sky
    Here we start watching pulsars over the entire sky.
    
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- py2.7/data-gen.py
+++ py2.7/data-gen.py
@@ -9,7 +9,7 @@ import numpy as np
 # I generate N pulsars in the solid angle defined by \theta and \phi randomly
 # scattered accross some length
 N = 36
-PulsarCoordinateRanges = np.array([ [10,40],[0.2,1.],[-0.5,0.5]])
+PulsarCoordinateRanges = np.array([ [10,40],[0, np.pi],[-np.pi, np.pi]])
 pulsarNoise = np.array([0.003, 0, 0])
 
 # Some time variables so that it is easier to read the code
