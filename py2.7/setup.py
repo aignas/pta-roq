b505ee9d47bf8e5383e5cf1231f9cc984f5a0882
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-02 13:50:23 +0100

    setup.py: add RB generator and enable openmp
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- py2.7/setup.py
+++ py2.7/setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("signal_model", ["signal_model.pyx"])]
+ext_modules = [
+    Extension("signal_model",  ["signal_model.pyx"]),
+    Extension("reduced_basis", ["reduced_basis.pyx"], extra_compile_args=['-fopenmp'], extra_link_args=['-fopenmp'])
+    ]
 
 setup(
   name = 'PTA signal modelling',
