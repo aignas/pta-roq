22137f729f96236a7874008d40f9d70be7227e5e
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-05 16:14:39 +0100

    Add more functions to linalg.
    Add more functions into linalg wrapper:
    
        * Inner Product
        * Norm
        * Grammian generation
        * Project a vector on another basis (the basis may be incomplete)
    
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- c++/src/linalg.cc
+++ c++/src/linalg.cc
@@ -1,7 +1,3 @@
-
-#include <vector>
-#include <valarray>
-
 // This needs guards, as otherwise the compiler doesn't know what to do. More on it
 // here:
 // http://www.lindonslog.com/programming/atlas-blas-lapack-linear-algebra-libraries/
@@ -12,9 +8,6 @@ extern "C" {
 
 // Local linear algebra routines to abstract things
 #include "linalg.hh"
-
-typedef std::vector<double> dvec;
-typedef std::valarray<double> darray;
 
 double dotProduct (dvec x, dvec y) {
     double r;
@@ -27,15 +20,57 @@ double dotProduct (dvec x, dvec y) {
     return r;
 }
 
-double innerProduct(dvec x, dvec y, darray A) {
+double innerProduct(dvec x, darray A, dvec y) {
     double r;
 
     // Fix it
-    if (x.size() == y.size()) {
-        r = cblas_ddot(x.size(), &x[0], 1, &y[0], 1);
+    if (x.size() == y.size() and x.size()*y.size() == A.size()) {
+        std::vector<double> tmp;
+        tmp.resize(x.size());
+        cblas_dgemv(CblasRowMajor, CblasNoTrans, x.size(), x.size(), 1, &A[0], 1, &y[0], 1, 1, &tmp[0], 1);
+        r = cblas_ddot(x.size(), &x[0], 1, &tmp[0], 1);
     } else {
         r = 0;
     }
 
     return r;
+}
+
+darray constructGrammian (std::vector<dvec> set, darray A) {
+    int N = set.size();
+
+    std::valarray<double> G (N*N);
+
+    for (unsigned int i = 0; i < N; i++) {
+        // The matrix is symmetric, hence calculate the diagonal terms here
+        G[i * (N + 1)] = innerProduct (set[i], A, set[i]);
+
+        for (unsigned int j = i+1; j < N; j++) {
+            // calculate the offdiagonal terms
+            G[N*j + i] = innerProduct (set[i], A, set[j]);
+            G[j + N*i] = G[i + N*j];
+        }
+    }
+
+    return G;
+}
+
+dvec projectionIntoSet (dvec projectee, std::vector<dvec> set, darray A, darray G) {
+    unsigned int N = set.size();
+    std::vector<double> tmp (projectee.size(), 0);
+    double c_i;
+
+    for (unsigned int i = 0; i < N; i++) {
+        c_i = 0;
+        for (unsigned int j = 0; j < N; j++) {
+            c_i += G[i + j*N] * innerProduct(projectee, A, set[j]);
+        }
+        cblas_daxpy(projectee.size(), c_i, &set[i][0], 1, &tmp[0], 1);
+    }
+
+    return tmp;
+}
+
+inline double norm (dvec x, darray A) {
+    return innerProduct(x, A, x);
 }
--- c++/src/linalg.hh
+++ c++/src/linalg.hh
@@ -25,7 +25,33 @@ double dotProduct (dvec x, dvec y);
  * This function internally uses ATLAS, so should be fast enough
  *
  * @param x A vector of doubles
- * @param y A vector of doubles
  * @param matrix a metrix of the same dimensionality as the vectors
+ * @param y A vector of doubles
  */
-double innerProduct (dvec x, dvec y, darray matrix);
+double innerProduct (dvec x, darray A, dvec y);
+
+/**
+ * Construct a grammian
+ *
+ * @param set A set of basis vectors
+ * @param A A matrix to evaluate the inner product with
+ */
+darray constructGrammian (std::vector<dvec> set, darray A);
+
+/**
+ * Project a vector onto basis vectors
+ *
+ * @param projectee The vector to project
+ * @param set The set of vector on which to project
+ * @param A The matrix for the inner product
+ * @param G The Grammian matrix for orthogonalization of the set
+ */
+dvec projectionIntoSet (dvec projectee, std::vector<dvec> set, darray A, darray G);
+
+/**
+ * Norm of a vector
+ *
+ * @param x A vector to norm
+ * @param A A matrix to norm it
+ */
+inline double norm (dvec x, darray A);
