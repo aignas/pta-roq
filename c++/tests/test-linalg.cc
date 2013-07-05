a5b13ecdca2fb41facdfe66171d76c4f2f243e7b
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-05 21:04:46 +0100

    Add test template files.
    These are not done, but started.
    
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- c++/tests/test-linalg.cc
+++ c++/tests/test-linalg.cc
@@ -24,18 +24,17 @@ int test_dotProduct() {
 }
 
 int test_innerProduct () {
-    std::vector<double> x (3), y(3);
-    x[0] = 1, y[0] = 5;
-    x[1] = 3, y[1] = 2;
-    x[2] = 2, y[2] = 1;
+    std::vector<double> x (2), y(2);
+    x[0] = 1, y[0] = -1;
+    x[1] = 2, y[1] = -3;
+    std::valarray<double> A (4);
+    A[0] = 1, A[1] = 3, A[2] = 2, A[3] = 1;
 
-    std::valarray<double> m;
-
-    double p = innerProduct(x,y,m);
+    double p = innerProduct(x, A, y);
     int r = 0;
 
     // Check the answer
-    if (p != 13) {
+    if (p != 20) {
         r++;
     }
 
---
+++ c++/tests/testrunner.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+#include "test-linalg.hh"
+
+int main () {
+}
