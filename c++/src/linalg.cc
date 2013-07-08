91dcdb0a2440fe69872f991abc9b6d01c287a90e
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-08 20:35:03 +0100

    Fix bugs and add matrix inversion
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- c++/src/linalg.cc
+++ c++/src/linalg.cc
@@ -3,11 +3,19 @@
 // http://www.lindonslog.com/programming/atlas-blas-lapack-linear-algebra-libraries/
 extern "C" {
 #include <cblas.h>
-#include <clapack.h>
+    // LU decomoposition of a general matrix
+    void dgetrf_(int* M, int *N, double* A, int* lda, int* IPIV, int* INFO);
+
+    // generate inverse of a matrix given its LU decomposition
+    void dgetri_(int* N, double* A, int* lda, int* IPIV, double* WORK, int*
+            lwork, int* INFO);
 }
 
 // Local linear algebra routines to abstract things
 #include "linalg.hh"
+#include "signal-sampling.hh"
+
+#include <iostream>
 
 double dotProduct (dvec& x, dvec& y) {
     double r;
@@ -20,56 +28,94 @@ double dotProduct (dvec& x, dvec& y) {
     return r;
 }
 
-double innerProduct(dvec& x, darray& A, dvec& y) {
-    double r;
+double innerProduct(dvec& x, dvec& A, dvec& y) {
+    std::vector<double> tmp (x.size(), 0);
 
-    // Fix it
-    if (x.size() == y.size() and x.size()*y.size() == A.size()) {
-        std::vector<double> tmp (x.size(), 0);
-        cblas_dgemv(CblasRowMajor, CblasNoTrans, x.size(), x.size(), 1, &A[0], 1, &y[0], 1, 1, &tmp[0], 1);
-        r = cblas_ddot(x.size(), &x[0], 1, &tmp[0], 1);
-    } else {
-        r = 0;
-    }
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, y.size(), y.size(), 1, &A[0], y.size(), &y[0], 1, 0, &tmp[0], 1);
+
+    double r = cblas_ddot(x.size(), &x[0], 1, &tmp[0], 1);
 
     return r;
 }
 
-darray constructGrammian (std::vector<dvec>& set, darray& A) {
+void constructGrammian (dvec &G, std::vector<dvec>& set, dvec& A) {
     unsigned int N = set.size();
-
-    std::valarray<double> G (N*N);
+    G.resize(N*N);
 
     for (unsigned int i = 0; i < N; i++) {
         // The matrix is symmetric, hence calculate the diagonal terms here
-        G[i * (N + 1)] = innerProduct (set[i], A, set[i]);
+        G.at(i * (N + 1)) = innerProduct (set[i], A, set[i]);
 
         for (unsigned int j = i+1; j < N; j++) {
             // calculate the offdiagonal terms
-            G[N*j + i] = innerProduct (set[i], A, set[j]);
-            G[j + N*i] = G[i + N*j];
+            G.at(N*j + i) = innerProduct (set[i], A, set[j]);
+            G.at(j + N*i) = G[i + N*j];
         }
     }
-
-    return G;
 }
 
-dvec projectionIntoSet (dvec& projectee, std::vector<dvec>& set, darray& A, darray& G) {
+void projectionResidual (dvec & projectee, std::vector<dvec> & set, 
+                         dvec & A, dvec & G, 
+                         dvec & coeffs) {
+    std::vector<double> tmp (projectee.size());
     unsigned int N = set.size();
-    std::vector<double> tmp (projectee.size(), 0);
-    double c_i;
+
+    // Calculate the missing coefficients
+    for (unsigned int i = coeffs.size(); i < N; i++) {
+        coeffs.push_back(0);
+
+        // Calculate the last coefficient, as we already are storring the rest
+        for (unsigned int j = 0; j < N - 1; j++) {
+            coeffs.at(i) += G[i + j*N] * innerProduct(projectee, A, set[j]);
+        }
+    }
 
     for (unsigned int i = 0; i < N; i++) {
-        c_i = 0;
-        for (unsigned int j = 0; j < N; j++) {
-            c_i += G[i + j*N] * innerProduct(projectee, A, set[j]);
-        }
-        cblas_daxpy(projectee.size(), c_i, &set[i][0], 1, &tmp[0], 1);
+        // Use BLAS to multiply by a constant
+        cblas_daxpy(projectee.size(), - coeffs[i], &set[i][0], 1, &projectee[0], 1);
     }
 
-    return tmp;
 }
 
-inline double norm (dvec& x, darray& A) {
-    return innerProduct(x, A, x);
+void linspace (std::vector<double> & array_out, double min, double max, const unsigned int N) {
+    array_out.resize(N);
+
+    // Check if the min and max are not mixed
+    if (min > max) {
+        double tmp = min;
+        min = max;
+        max = tmp;
+    }
+
+    // Push back the first element
+    array_out[0] = min;
+
+    if (N != 1) {
+        const double dx = (max - min) / (N - 1);
+
+        // push back the intermediate elements
+        for (unsigned int i = 1; i < N - 1; i++) {
+            array_out.at(i) = min + i*dx;
+        }
+
+        // Push back the last element
+        array_out.at(N-1) = max;
+    }
+}
+
+void inverse (std::vector<double> & A) {
+    int N = sqrt(A.size());
+
+    int INFO;
+
+    std::vector<int> IPIV (N+1);
+    int LWORK = A.size();
+    std::vector<double> WORK (LWORK);
+
+    // Execute the LAPACK routines
+    dgetrf_(&N, &N, &A[0], &N, &IPIV[0], &INFO);
+    dgetri_(&N, &A[0], &N, &IPIV[0], &WORK[0], &LWORK, &INFO);
+
+    IPIV.clear();
+    WORK.clear();
 }
--- c++/src/linalg.hh
+++ c++/src/linalg.hh
@@ -28,15 +28,16 @@ double dotProduct (dvec& x, dvec& y);
  * @param matrix a metrix of the same dimensionality as the vectors
  * @param y A vector of doubles
  */
-double innerProduct (dvec& x, darray& A, dvec& y);
+double innerProduct (dvec& x, dvec& A, dvec& y);
 
 /**
  * Construct a grammian
  *
+ * @param G An output array
  * @param set A set of basis vectors
  * @param A A matrix to evaluate the inner product with
  */
-darray constructGrammian (std::vector<dvec>& set, darray& A);
+void constructGrammian (dvec &G, std::vector<dvec>& set, dvec& A);
 
 /**
  * Project a vector onto basis vectors
@@ -44,9 +45,14 @@ darray constructGrammian (std::vector<dvec>& set, darray& A);
  * @param projectee The vector to project
  * @param set The set of vector on which to project
  * @param A The matrix for the inner product
- * @param G The Grammian matrix for orthogonalization of the set
+ * @param G_inv The inverse Grammian matrix for orthogonalization of the set
+ * @params coeffs The coefficients for speeding up the calculation. If there are
+ *   projection coefficients already calculated, then we can use them to speed up the
+ *   calculation
  */
-dvec projectionIntoSet (dvec& projectee, std::vector<dvec>& set, darray& A, darray& G);
+void projectionResidual (dvec & projectee, std::vector<dvec> & set, 
+                         dvec & A, dvec & G_inv, 
+                         dvec & coeffs);
 
 /**
  * Norm of a vector
@@ -54,4 +60,24 @@ dvec projectionIntoSet (dvec& projectee, std::vector<dvec>& set, darray& A, darr
  * @param x A vector to norm
  * @param A A matrix to norm it
  */
-inline double norm (dvec& x, darray& A);
+inline double norm (dvec& x, dvec& A) {
+    return innerProduct(x, A, x);
+}
+
+/**
+ * Matlab like linalg function
+ *
+ * @param array_out The output array (passed as a reference)
+ * @param min A double representing a min value
+ * @param max A double representing a max value
+ * @param N Number of points in the array
+ */
+void linspace (dvec & array_out, double min, double max, const unsigned int N);
+
+/**
+ * Invert a matrix
+ *
+ * @param A a matrix with Row Major arrangement
+ * @param The dimensionality of the matrix (it is assumed we have a square matrix)
+ */
+void inverse (dvec & A);
