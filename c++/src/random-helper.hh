8fbe616c191d03998c20b02d8a35c87bb9155f58
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-08 20:35:32 +0100

    Add integer generator
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- c++/src/random-helper.hh
+++ c++/src/random-helper.hh
@@ -14,6 +14,17 @@ double random_uniform (const double LO = 0,
                        const unsigned long int seed = 0);
 
 /**
+ * Generate a random number between [0,HI] if the lower limit is unspecified
+ *
+ * @param HI The high value of the range
+ * @param LO The low value of the range (default 0)
+ * @param seed The seed value (default 0)
+ */
+long random_uniform_int (const long LO,
+                         const long HI,
+                         const unsigned long int seed = 0);
+
+/**
  * Free the random number generator
  */
 void random_uniform_free (void);
