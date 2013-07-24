571df437855554722c724e36c08e5aae45194bbf
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2013-07-24 17:56:04 +0100

    Update the test suite
    Signed-off-by: Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>

--- c++/src/test/iocsv.cc
+++ c++/src/test/iocsv.cc
@@ -22,7 +22,7 @@ int test_csvPulsar () {
     int r = 0;
 
     // Check the pulsar size only at the moment
-    if (pulsars_read.size() != 36) {
+    if (pulsars_read.size() != pulsarNumber) {
         // FIXME check whether the parameters of the two arrays match.
         r++;
     }
@@ -41,9 +41,33 @@ int test_csvSources () {
 }
 
 int test_csvSchedule () {
+    // We need some pulsar data
+    test_csvPulsar ();
+
+    std::vector<Pulsar> pulsars;
+    std::string filename_pulsar = "data_test/test_csv2pulsar.csv",
+                filename_sched = "data_test/test_csv2schedule.csv";
+
+    // Read some pulsar data
+    csv2pulsar (filename_pulsar, pulsars, ",");
+
+    double t_final = 5*52, dt_min = 2, dt_max = 2;
+    std::vector<unsigned short> indices, indices_read;
+
+    const unsigned int pulsarNumber = 36;
+    std::vector<double> Times, Times_read, t_init (pulsarNumber, 0);
+
+    pulsarGrid::generateSchedule (t_init, t_final, dt_min, dt_max, indices, Times);
+
+    arraysShortDouble2csv (filename_sched, indices, Times, ",");
+    csv2arraysShortDouble (filename_sched, indices_read, Times_read, ",");
+
     int r = 0;
 
-    if (true) {
+    if (indices.size() != indices.size() or Times.size() != Times_read.size()) {
+        std::cerr << indices.size() << " != " << indices.size() 
+                  << " or " << Times.size() << " != " << Times_read.size()
+                  << std::endl;
         r++;
     }
 
